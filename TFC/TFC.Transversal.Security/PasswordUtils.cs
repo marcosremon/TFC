0c188f1e983cf258e06dc50fc7c07be9e1b8326c
marcos.remon <marcos.remon@kintech-engineering.com>
2025-04-09 10:03:17 +0200

    Clase PasswordUtils añadida

--- TFC/TFC.Transversal.Security/PasswordUtils.cs
+++ TFC/TFC.Transversal.Security/PasswordUtils.cs
@@ -1,7 +1,97 @@
-﻿namespace TFC.Transversal.Security
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TFC.Transversal.Security
 {
     public class PasswordUtils
     {
+        public static string publickey = "1234567812345678"; // 16 bytes para AES-128
+        public static string secretkey = "8765432187654321"; // 16 bytes para AES-128
+        public static byte[] secretkeyByte = Encoding.UTF8.GetBytes(secretkey);
+        public static byte[] publickeybyte = Encoding.UTF8.GetBytes(publickey);
+        public static byte[] result;
 
+        public static string PasswordEncoder(string password)
+        {
+            try
+            {
+                string textToEncrypt = password;
+                byte[] inputbyteArray = Encoding.UTF8.GetBytes(textToEncrypt);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = publickeybyte;
+                    aes.IV = secretkeyByte;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                            cs.FlushFinalBlock();
+                            result = ms.ToArray();
+                        }
+                    }
+                }
+                return Convert.ToBase64String(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+
+        public static string PasswordDecoder(byte[] encryptedPassword)
+        {
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = publickeybyte;
+                    aes.IV = secretkeyByte;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(encryptedPassword, 0, encryptedPassword.Length);
+                            cs.FlushFinalBlock();
+                            result = ms.ToArray();
+                        }
+                    }
+                }
+                return Encoding.UTF8.GetString(result);
+            }
+            catch (CryptographicException ex)
+            {
+                throw new Exception("Error al desencriptar la contraseña. Asegúrate de que los datos encriptados y las claves sean correctos.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+        public static bool IsPasswordEncrypted(string password)
+        {
+            try
+            {
+                byte[] decodedBytes = Convert.FromBase64String(password);
+                return decodedBytes.Length % 16 == 0;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        public static string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
     }
 }
