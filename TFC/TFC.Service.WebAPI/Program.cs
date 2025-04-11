70783aca483b76d3faacd30a825708d150a39ab0
marcos.remon <marcos.remon@kintech-engineering.com>
2025-04-11 12:53:27 +0200

    clase jwt añadida mas toda su config y clase dependencyInjection creada

--- TFC/TFC.Service.WebAPI/Program.cs
+++ TFC/TFC.Service.WebAPI/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TFC.Infrastructure.Persistence.Dependencies;
+using TFC.Service.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configuración mínima
 builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,6 +19,27 @@ builder.Services.AddSingleton<ApplicationDbContext>(provider =>
     var config = provider.GetRequiredService<IConfiguration>();
     return new ApplicationDbContext(config);
 });
+
+// Inicializa JwtUtils con la configuración
+JwtUtils.Initialize(builder.Configuration);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["jwt:Issuer"],
+            ValidAudience = builder.Configuration["jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"]))
+        };
+    });
+
+// Registrar aplicaciones services
+builder.Services.AddInfrastructureServices();
 
 var app = builder.Build();
 
@@ -22,6 +51,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
