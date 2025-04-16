0c0ca7bad18d1c8afe5b6413ed5cfd6fae2b131e
Marcos <mremonb@iesch.org>
2025-04-16 22:15:25 +0200

    comprobaciones exercises movidas al application

--- TFC/TFC.Application.Main/ExerciseApplication.cs
+++ TFC/TFC.Application.Main/ExerciseApplication.cs
@@ -17,21 +17,56 @@ namespace TFC.Application.Main
 
         public async Task<AddExerciseResponse> AddExercise(AddExerciseRequest addExerciseRequest)
         {
+            if (addExerciseRequest == null
+                || addExerciseRequest.UserId == null
+                || addExerciseRequest.RoutineId == null
+                || addExerciseRequest.DayName == null
+                || string.IsNullOrEmpty(addExerciseRequest.ExerciseName))
+            {
+                return null;
+            }
+
             return await _exerciseRepository.AddExercise(addExerciseRequest);
         }
 
         public async Task<DeleteExerciseResponse> DeleteExercise(DeleteExerciseRequest deleteExerciseRequest)
         {
+            if (deleteExerciseRequest == null
+                || deleteExerciseRequest.UserId == null
+                || deleteExerciseRequest.RoutineId == null
+                || deleteExerciseRequest.DayName == null
+                || string.IsNullOrEmpty(deleteExerciseRequest.ExerciseName))
+            {
+                return null;
+            }
+
             return await _exerciseRepository.DeleteExercise(deleteExerciseRequest);
         }
 
         public async Task<GetExercisesByDayNameResponse> GetExercisesByDayName(GetExercisesByDayNameRequest getExercisesByDayNameRequest)
         {
+            if (getExercisesByDayNameRequest == null
+                || getExercisesByDayNameRequest.UserId == null
+                || getExercisesByDayNameRequest.RoutineId == null
+                || getExercisesByDayNameRequest.DayName == null)
+            {
+                return null;
+            }
+
             return await _exerciseRepository.GetExercisesByDayName(getExercisesByDayNameRequest);
         }
 
         public async Task<UpdateExerciseResponse> UpdateExercise(UpdateExerciseRequest updateExerciseRequest)
         {
+            if (updateExerciseRequest == null
+                || updateExerciseRequest.UserId == null
+                || updateExerciseRequest.RoutineId == null
+                || updateExerciseRequest.DayName == null
+                || string.IsNullOrEmpty(updateExerciseRequest.ExerciseName))
+            {
+                return null;
+            }
+
             return await _exerciseRepository.UpdateExercise(updateExerciseRequest);
         }
     }
--- TFC/TFC.Service.WebAPI/Controllers/ExerciseController.cs
+++ TFC/TFC.Service.WebAPI/Controllers/ExerciseController.cs
@@ -18,84 +18,49 @@ namespace TFC.Service.WebApi.Controllers
         [HttpPost("AddExercise")]
         public async Task<ActionResult<AddExerciseResponse>> AddExercise([FromBody] AddExerciseRequest addExerciseRequest)
         {
-            if (addExerciseRequest == null
-                || addExerciseRequest.UserId == null
-                || addExerciseRequest.RoutineId == null
-                || addExerciseRequest.DayName == null
-                || string.IsNullOrEmpty(addExerciseRequest.ExerciseName))
-            {
-                return BadRequest();
-            }
-
             AddExerciseResponse response = await _exerciseApplication.AddExercise(addExerciseRequest);
-            if (response.IsSuccess)
+            if (!response.IsSuccess || response == null)
             {
-                return Ok(response);
+                return BadRequest(response.Message);
             }
 
-            return BadRequest(response.Message);
+            return Ok(response);
         }
 
         [HttpPut("UpdateExercise")]
         public async Task<ActionResult<UpdateExerciseResponse>> UpdateExercise([FromBody] UpdateExerciseRequest updateExerciseRequest)
         {
-            if (updateExerciseRequest == null
-                || updateExerciseRequest.UserId == null
-                || updateExerciseRequest.RoutineId == null
-                || updateExerciseRequest.DayName == null
-                || string.IsNullOrEmpty(updateExerciseRequest.ExerciseName))
-            {
-                return BadRequest();
-            }
-
             UpdateExerciseResponse response = await _exerciseApplication.UpdateExercise(updateExerciseRequest);
-            if (response.IsSuccess)
+            if (!response.IsSuccess || response == null)
             {
-                return Ok(response);
+                return BadRequest(response.Message);
             }
 
-            return BadRequest(response.Message);
+            return Ok(response);
         }
 
         [HttpDelete("DeleteExercise")]
         public async Task<ActionResult<DeleteExerciseResponse>> DeleteExercise([FromBody] DeleteExerciseRequest deleteExerciseRequest)
         {
-            if (deleteExerciseRequest == null
-                || deleteExerciseRequest.UserId == null
-                || deleteExerciseRequest.RoutineId == null
-                || deleteExerciseRequest.DayName == null
-                || string.IsNullOrEmpty(deleteExerciseRequest.ExerciseName))
-            {
-                return BadRequest();
-            }
-
             DeleteExerciseResponse response = await _exerciseApplication.DeleteExercise(deleteExerciseRequest);
-            if (response.IsSuccess)
+            if (!response.IsSuccess || response == null)
             {
-                return Ok(response);
+                return BadRequest(response.Message);
             }
 
-            return BadRequest(response.Message);
+            return Ok(response);
         }
 
         [HttpPost("GetExercisesByDayName")]
         public async Task<ActionResult<GetExercisesByDayNameResponse>> GetExercisesByDayName([FromBody] GetExercisesByDayNameRequest getExercisesByDayNameRequest)
         {
-            if (getExercisesByDayNameRequest == null
-                || getExercisesByDayNameRequest.UserId == null
-                || getExercisesByDayNameRequest.RoutineId == null
-                || getExercisesByDayNameRequest.DayName == null)
-            {
-                return BadRequest();
-            }
-
             GetExercisesByDayNameResponse response = await _exerciseApplication.GetExercisesByDayName(getExercisesByDayNameRequest);
-            if (response.IsSuccess)
+            if (!response.IsSuccess || response == null)
             {
-                return Ok(response);
+                return BadRequest(response.Message);
             }
 
-            return BadRequest(response.Message);
+            return Ok(response);
         }
     }
 }
